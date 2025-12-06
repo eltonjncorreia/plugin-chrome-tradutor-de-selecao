0beb226ce8f52382466089435241b64c12f7999e
eltonjncorreia <eltonjncorreia@gmail.com>
2025-12-06 10:56:11 -0300

    fix: rename file background.js

--- translate_selected/backgroud.js
+++
@@ -1,6 +0,0 @@
-// Configurações padrão ao instalar
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ targetLang: "pt" });
-
-  console.log("Extensão Tradutor de Seleção instalada com sucesso!");
-});
